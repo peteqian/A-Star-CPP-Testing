5d1227efd499bfb08cac876b5f06b8e27b78cad1
thepancakeman <lhq036@uowmail.edu.au>
2021-05-10 09:49:39 +1000

    Updated to add NoPath
    NoPath file works, however will require revising due to incorrect data being read.

--- test/SimTests.cpp
+++ test/SimTests.cpp
@@ -1,16 +1,11 @@
-#include "../ass2-refactor.h"
+#include "../ass3-refactor.h"
 #include <gtest/gtest.h>
-<<<<<<< Updated upstream
-
-TEST(TestCase1, subTest1){
-    Sim sim;
-    sim.run();
-=======
 
 class SimulationTest : public::testing::Test{
     protected:
         SimulationTest() = default;
         Simulation simulation;
+        
 };
 
 TEST_F(SimulationTest, OpenExistingFile){
@@ -18,18 +13,21 @@ TEST_F(SimulationTest, OpenExistingFile){
 }
 
 TEST_F(SimulationTest, OpenNonExistingFile){
-    ASSERT_EQ(1,simulation.openFile("Ass2.txt"));
+    ASSERT_EQ(0,simulation.openFile("Ass2.txt"));
 }
 
 TEST_F(SimulationTest, correctRun){
     simulation.openFile("Ass3.txt");
-    simulation.run();
-    ASSERT_TRUE(true);
+    ASSERT_EQ(1,simulation.run() );
+}
+
+TEST_F(SimulationTest, noPath){
+    simulation.openFile("NoPath.txt");
+    ASSERT_EQ(1,simulation.run());
 }
 
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
->>>>>>> Stashed changes
 }
